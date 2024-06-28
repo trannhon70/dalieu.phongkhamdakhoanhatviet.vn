acf8b477f9ede30b43f6ad7929a13ead334adeb3
trannhon70 <kevintran351996@gmail.com>
2024-06-28 13:59:11 +0700

    update config

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ define("DB_NAME", "phongkhamdakhanhatviet.vn");
 
 
 // define("DB_HOST", "localhost");
-// define("DB_USER", "benh_xahoi_root");
-// define("DB_PASS", "Ie@giB4+inp8oH^A");
-// define("DB_NAME", "benh_xh_phongkhamdakhoanhatviet");   
+// define("DB_USER", "dali_root");
+// define("DB_PASS", "5pjf!plPGwz4yDcN");
+// define("DB_NAME", "dali_phongkhamdakhoanhatviet");   
 ?>
